ee9cb82afeffce33b55ea0dde577fdd75bd0abe4
adfansong <chenyouzhou99@gmail.com>
2020-06-13 14:38:14 +0800

    add

--- assets/script/nativePlugin/TestNativePlugin.ts
+++ assets/script/nativePlugin/TestNativePlugin.ts
@@ -8,21 +8,43 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class TestNativePlugin extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
 
-    @property
-    text: string = 'hello';
-
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad () {
+        let w = window as any;
+        w.__native = (s:string)=>{
+            cc.log('native msg:', s);
 
-    start () {
+            let arr = s.split(';');
 
+            let type = arr[0];
+            if (type == 'test_ok') {
+                this.label.string = 'test ok:' + arr[1];
+            }
+        };
     }
 
-    // update (dt) {}
+    start () {
+        this.sendMsgToPlugin('test');
+    }
+
+    
+    //#region F
+    sendMsgToPlugin(msg:string) {
+        if (CC_JSB) {
+            let os = cc.sys.os;
+
+            if (os == cc.sys.OS_ANDROID) {
+                jsb.reflection.callStaticMethod('com/bw/plugin/NativePlugin', 'sendMsgToPlugin', '(Ljava/lang/String;)V', msg);
+            } else if (os == cc.sys.OS_IOS) {
+                jsb.reflection.callStaticMethod('APIObject', 'sendMsgToPlugin:', msg);
+            }
+        }
+    }
+    //#endregion
 }
